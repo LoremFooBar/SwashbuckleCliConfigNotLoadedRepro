1b2c8e2eb911410102359917459c489bce534508
Amit Edelshtein <amit@camilyo.com>
2021-07-30 18:08:34 -0400

    Initial commit

---
+++ SwaggerCliTest/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerCliTest.Controllers
+{
+    [Controller]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public List<int> Get() => new List<int> { 1, 2, 3 };
+    }
+}
